05cb5d782b1d05c88ba5aac3475e8c60e766de8d
elcentaurx <luismarquez.ortiz94@gmail.com>
2022-06-23 17:12:10 -0500

    refactor

---
+++ config/prod.exs
@@ -0,0 +1,19 @@
+use Mix.Config
+
+config :Melp, MelpWeb.Endpoint,
+  load_from_system_env: true,
+  http: [port: {:system, "PORT"}],
+  server: true,
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json"
+
+config :Melp, Melp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}",
+  database: "", # Works around a bug in older versions of ecto. Doesn't hurt for other versions.
+  ssl: true,
+  pool_size: 2 # Free tier
+
+# Do not print debug messages in production
+config :logger, level: :info
